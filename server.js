fc6cfcf50e9faa310c325dd776c83e46883c2c9f
jokeeras <jokeeras@yahoo.com>
2019-06-08 17:04:12 +0300

    Adding port

--- server.js
+++ server.js
@@ -41,8 +41,8 @@ app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db) }
 app.put('/image', (req, res) => { image.handleImage(req, res, db) })
 app.post('/imageUrl', (req, res) => { image.handleApiCall(req, res) })
 
-app.listen(3001, () => {
-    console.log('App is running on port: 3001')
+app.listen(process.env.PORT || 3001, () => {
+    console.log('App is running on port: ${process.env.PORT}')
 })
 
 /* 
