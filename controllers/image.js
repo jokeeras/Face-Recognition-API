5f50cecf085ea5c53adde1a5cb3fbb5aafe01ad4
jokeeras <jokeeras@yahoo.com>
2019-06-08 16:35:55 +0300

    Finished server

---
+++ controllers/image.js
@@ -0,0 +1,30 @@
+const Clarifai = require('clarifai');
+
+// ADD clarifai API key
+const app = new Clarifai.App({
+    apiKey: '7f083273f1fb42c28220fb735fe709fc'
+   });
+
+const handleApiCall = (req, res) => {
+    app.models.predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+    .then(data => {
+        res.json(data);
+    })
+    .catch(err => res.status(400).json('Unable to towrk with API'))
+}
+
+const handleImage = (req, res, db) => {
+    const { id } = req.body;
+    db('users').where('id', '=', id)
+    .increment('entries', 1)
+    .returning('entries')
+    .then(entries => {
+        res.json(entries[0]);
+    })
+    .catch(err => res.status(400).json('Unable to get entries'))
+}
+
+module.exports = {
+    handleImage,
+    handleApiCall
+}
--- server.js
+++ server.js
@@ -4,6 +4,11 @@ const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
 const knex = require('knex');
 
+const signin = require('./controllers/signin');
+const register = require('./controllers/register');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
+
 const db = knex({
     client: 'pg',
     connection: {
@@ -21,94 +26,24 @@ app.use(cors());
 app.use(bodyParser.json());
 
 // ROOT
-app.get('/', (req, res) => {
-    res.send(database.users);
-})
+app.get('/', (req, res) => {res.send(database.users);})
 
 // SIGN IN
-app.post('/signin', (req,res) => {
-    db.select('email', 'hash').from('login')
-    .where('email', '=', req.body.email)
-        .then(data => {
-            const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
-            if(isValid) {
-                return db.select('*').from('users')
-                    .where('email', '=', req.body.email)
-                    .then(user => {
-                        res.json(user[0])
-                    })
-                    .catch(err => res.status(400).json('Unable to get user'))
-            } else {
-                res.status(400).json('Wrong credentials')
-            }
-        })
-        .catch(err => res.status(400).json('Wrong credentials'))
-})
+app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt) })
 
 // REGISTER
-app.post('/register', (req, res) => {
-    const { email, name, password } = req.body;
-    const hash = bcrypt.hashSync(password);
-    db.transaction(trx => {
-        trx.insert({
-            hash: hash,
-            email: email,
-        })
-        .into('login')
-        .returning('email')
-        .then(loginEmail => {
-            return trx('users')
-            .returning('*')
-            .insert({
-                email: loginEmail[0],
-                name: name,
-                joined: new Date()
-            })
-            .then(user => {
-                res.json(user[0]);
-            })
-        })
-        .then(trx.commit)
-        .catch(trx.rollback)
-    })
-    .catch(err => res.status(400).json('unable to register'))
-})
+app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
 
 // PROFILE
-app.get('/profile/:id', (req, res) => {
-    const { id } = req.params;
-    db.select('*').from('users').where({id})
-    .then(user => {
-        if(user.length) {
-            res.json(user[0])
-        } else {
-            res.status(400).json('Not found')
-        }
-    })
-    .catch(err => res.status(400).json('Error getting user'))
-})
+app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db) })
 
 // IMAGE
-app.put('/image', (req, res) => {
-    const { id } = req.body;
-    db('users').where('id', '=', id)
-    .increment('entries', 1)
-    .returning('entries')
-    .then(entries => {
-        res.json(entries[0]);
-    })
-    .catch(err => res.status(400).json('Unable to get entries'))
-})
-
-
-
+app.put('/image', (req, res) => { image.handleImage(req, res, db) })
+app.post('/imageUrl', (req, res) => { image.handleApiCall(req, res) })
 
 app.listen(3001, () => {
-    console.log('app is running on port 3001')
+    console.log('App is running on port: 3001')
 })
-
-
-
 
 /* 
 / --> res = this is working
